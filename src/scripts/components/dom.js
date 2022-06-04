4ed34ac8841f536699f0c74d9184cfebfc101374
Jordan <100964858+j0j032@users.noreply.github.com>
2022-06-04 15:41:32 +0100

    add begining of display logic 🚧

--- src/scripts/components/dom.js
+++ src/scripts/components/dom.js
@@ -1,1 +1,20 @@
+const { photographersSection } = require("./domLinker");
 
+const createName = (tag, data) => {
+  const cardName = document.createElement(tag);
+  cardName.textContent = `${data.name}`;
+
+  photographersSection.appendChild(cardName);
+};
+
+const createLocation = (tag, data) => {
+  const cardLocation = document.createElement(tag);
+  cardLocation.textContent = `${data.city}, ${data.country}`;
+
+  photographersSection.appendChild(cardLocation);
+};
+
+module.exports = {
+  createName,
+  createLocation,
+};
--- src/scripts/pages/index.js
+++ src/scripts/pages/index.js
@@ -1,8 +1,17 @@
 const api = require("../components/api");
+const factory = require("../components/dom");
+
+const displayCards = (data) => {
+  data.forEach((photographer) => {
+    factory.createName("h1", photographer);
+    factory.createLocation("a", photographer);
+  });
+};
 
 const init = async () => {
   const photographers = await api.getPhotographers();
   console.log(photographers);
+  displayCards(photographers);
 };
 
 init();
