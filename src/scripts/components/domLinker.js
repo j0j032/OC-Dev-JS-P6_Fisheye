cfc0df5dfb50df806f117bc8ff23c1c4d06fcfd7
Jordan <100964858+j0j032@users.noreply.github.com>
2022-06-10 11:18:50 +0100

    Ticket DJS-79 done  🔀  Refactor media factories, scss and JS naming 🎨 update comments 🗯

--- src/scripts/components/domLinker.js
+++ src/scripts/components/domLinker.js
@@ -3,6 +3,6 @@ module.exports = {
   photographHeader: document.querySelector(".photograph-header"),
   photogHeadDescription :document.querySelector('.photograph-header__description'),
   photogHeadPic :document.querySelector('.photograph-header__profilePic-Container'),
-  priceContainer :document.querySelector('.likesAndPriceContainer__price'),
-  imgContainer : document.querySelector('.img-container')
+  priceContainer :document.querySelector('.totalLikes-container'),
+  mediasContainer : document.querySelector('.medias__container')
 };
--- src/scripts/factories/photographer.js
+++ src/scripts/factories/photographer.js
@@ -4,7 +4,7 @@ const { createElements, setParent } = require("../components/dom")
 const domLink = require("../components/domLinker")
 
 /**
- * Factory to fill a photographer profile informations if display on hompage, display as a 'card' else if on photographer profile page display as a 'header'
+ * Factory to fill a photographer 'Profile card' informations on homepage
  * @param {object} data 
  */
 const getPhotographerCard = (data) => {
@@ -29,9 +29,8 @@ const getPhotographerCard = (data) => {
         createElements('img', null,"card__pic",profilePictureAlt, profilePicture, profilePictureAlt, cardLink )
         createElements("h2", name, "card__name",name, null,null, cardLink)
       }
-      /**
-       * To create a card and get all the elements together 
-       */
+
+       //To create a card and get all the elements together 
         const createCard = () =>{
         const card = document.createElement("div")
         setParent(card, "card", null,null )
@@ -45,15 +44,19 @@ const getPhotographerCard = (data) => {
     createCard()
     } 
     
-    
+    /**
+     * To fill profile header on photographer page
+     * @param {object} data 
+     */
     const getPhotographerProfileDetails = (data) => {
       // turn data into var easy reusable
-  const {name, id, city, country, tagline, price, portrait} = data
-  const photographerLocation = `${city}, ${country}`
-  const profilePicture = `/src/assets/photographers/${portrait}`;
-  const profilePictureAlt = `Portrait de ${name}`;
-  const photographerPricing = `${price}€/jour`
+      const {name, id, city, country, tagline, price, portrait} = data
+      const photographerLocation = `${city}, ${country}`
+      const profilePicture = `/src/assets/photographers/${portrait}`;
+      const profilePictureAlt = `Portrait de ${name}`;
+      const photographerPricing = `${price}€/jour`
 
+      // Factory to inject profile details
       const createProfileHeader = () => {
         createElements('img', null,"card__pic",profilePictureAlt, profilePicture, profilePictureAlt, domLink.photogHeadPic)
         createElements("h1", name, "card__name",name, null,null, domLink.photogHeadDescription)
@@ -64,43 +67,50 @@ const getPhotographerCard = (data) => {
       createProfileHeader()
     }
     
+    /**
+     * Factory to fill medias for each photographer on photographer page
+     * @param {object} data 
+     */
     const getPhotographersMedias = (data) => {
+      // turn data into var easy reusable
       const {date, id, image,video, likes, photographerId, price, title} = data
       const mediaI = `../src/assets/medias/${image}`;
       const mediaV = `../src/assets/medias/${video}`;
       const mediaAlt = `${title}`;
       const mediaLikes = `${likes}`
-      const heartIcon = "fa-solid fa-heart"
 
-      const createPictureCard = () => {
+      //to display one media 'Card' (media+infos)
+      const createMediaCard = () => {
         const mediaContainer = document.createElement("div")
-        setParent(mediaContainer, "media-container", null, null)
-        domLink.imgContainer.appendChild(mediaContainer)
-        if(image){createElements('img', null, "Picture", mediaAlt, mediaI, mediaAlt, mediaContainer )}
+        setParent(mediaContainer, "media__container", null, null)
+        domLink.mediasContainer.appendChild(mediaContainer)
+        //Logic to inject by media type
+        if(image){
+          createElements('img', null, "media__img", mediaAlt, mediaI, mediaAlt, mediaContainer )
+        }
         else if(video){
           const newVideo = document.createElement("video")
-          setParent(newVideo, "video", mediaAlt, null)
+          setParent(newVideo, "media__video", mediaAlt, null)
           mediaContainer.appendChild(newVideo)
           createElements('source', null, null, mediaAlt, mediaV, mediaAlt, newVideo, "video/mp4")
         }
 
-        const mediaTitleContainer = document.createElement("div")
-        setParent(mediaTitleContainer, "media-container__Title", null,null)
-        mediaContainer.appendChild(mediaTitleContainer)
+        // informations container (total of likes and photographer pricing)
+        const mediaInfosContainer = document.createElement("div")
+        setParent(mediaInfosContainer, "media__container--infos", null,null)
+        mediaContainer.appendChild(mediaInfosContainer)
 
-        createElements('p', mediaAlt, "media-title", mediaAlt, null, mediaAlt, mediaTitleContainer, null)
+        createElements('p', mediaAlt, "media__title", mediaAlt, null, mediaAlt, mediaInfosContainer, null)
 
         const mediaLikesContainer = document.createElement("div")
-        setParent(mediaLikesContainer, "media-container__likes", null, null)
-        mediaTitleContainer.appendChild(mediaLikesContainer)
-        createElements('p', mediaLikes, "media-likes", mediaLikes, null, mediaLikes, mediaLikesContainer, null)
+        setParent(mediaLikesContainer, "media__container--likes", null, null)
+        mediaInfosContainer.appendChild(mediaLikesContainer)
+
+        createElements('p', mediaLikes, "media__likes", mediaLikes, null, mediaLikes, mediaLikesContainer, null)
         createElements('i',null, "fa-heart","icon coeur", null, "icon coeur", mediaLikesContainer, null, "fa-solid")
         
-
-
-
       }
-      createPictureCard()
+      createMediaCard()
     }
 
 module.exports ={
